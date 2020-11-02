35d243c61bcc2db2aaaf5a18e420a13e342de578
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-02 13:53:27 +0100

    JS exercice 4

---
+++ Exo4/script.js
@@ -0,0 +1,6 @@
+function controle(form) {
+    var firstName = document.form.firstName.value;
+    var lastName = document.form.lastName.value;
+    var city = document.form.city.value;
+    alert('Bonjour ' +" " + firstName +" " + lastName +" " + 'de ' + city);
+}
