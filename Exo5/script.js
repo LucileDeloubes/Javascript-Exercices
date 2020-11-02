9def1907fd1399febc190e759eeb37bcfc1fa3fa
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-02 14:15:46 +0100

    JS exercice 5

---
+++ Exo5/script.js
@@ -0,0 +1,5 @@
+function controle(form) {
+    var firstNumber = document.form.firstNumber.value;
+    var secondNumber = document.form.secondNumber.value;
+    alert('Le résultat est '+ firstNumber / secondNumber);
+}
