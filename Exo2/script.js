133932b393a6a1bc118ffd3f7da5e76674e3aa34
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-02 11:48:22 +0100

    Javascript Exercice 2 corrigé

---
+++ Exo2/script.js
@@ -0,0 +1,6 @@
+    var lastName = 'Doe'; 
+    var firstName = 'John';
+    var city = 'New-York';
+    alert('Nom : ' + lastName + '\n' + 'Prénom : ' + firstName + '\n' + 'Ville : ' + city)
+// le \n permet le saut de ligne et les + permettent d'ajouter des variables
+    
