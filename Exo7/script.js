c2c731646106cd7948d0b80f4510b4a34bc207ee
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-02 14:57:32 +0100

    JS exercice 7 conditions if et else

---
+++ Exo7/script.js
@@ -0,0 +1,11 @@
+function controle(form) {
+    var age = document.form.age.value;
+    var majeur = age >= 18;
+    var mineur = age < 18;
+    if (majeur === true){
+        alert("Vous êtes majeur hourra !")
+    } else {
+        alert("Vous êtes mineur bouuuuuuuh ! ")
+    }
+}
+// réussir à comparer 2 valeurs et à donner des conditions d'affichage avec IF et ELSE , indiqué si la condition est vraie ou fausse.
