40eaa946a4c68c6dccae0eeff17803f3cbcba116
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-03 16:52:55 +0100

    correction exo 7

--- Exo7/script.js
+++ Exo7/script.js
@@ -2,10 +2,15 @@ function controle() {
     var age = document.getElementById('age').value;
     var majeur = age >= 18;
     var mineur = age < 18;
-    if (majeur === true){
+    if (age >= 18 && age < 150){
         alert("Vous êtes majeur hourra !")
-    } else {
-        alert("Vous êtes mineur bouuuuuuuh ! ")
+    }
+    else if (age >= 0 && age < 18 ){
+        alert("Vous êtes mineur bouuuuuuuh ! ! ")
+    }
+
+    else {
+        alert("Vous êtes un méchant mythomane ! ")
     }
 }
 // réussir à comparer 2 valeurs et à donner des conditions d'affichage avec IF et ELSE , indiqué si la condition est vraie ou fausse.
