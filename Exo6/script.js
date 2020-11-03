922c4bc2c01c56abec31ff42dd6b2db6a2e4d2bb
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-03 10:04:16 +0100

    modif calcul sur la même variable

--- Exo6/script.js
+++ Exo6/script.js
@@ -1,10 +1,9 @@
 function controle() {
     var shoes = document.getElementById('shoes').value;
     var birthYear = document.getElementById('birthYear').value;
-    var resultat1 = (shoes*2) + 5;
-    var resultat2 = (resultat1 * 50) - birthYear;
-    var resultat3 = resultat2 + 1769;
-    alert('TADAAAAAM ! Le résultat est..... ' + resultat3 )  ;
+    var resultat1 = ((((shoes*2)+5)*50) - birthYear) + 1769 ;
+  69;
+    alert('TADAAAAAM ! Le résultat est..... ' + resultat1 )  ;
 }
 
-// décomposer le calcul au fur et à mesure
+// Mettre tout le calcul sur une même variablme, utiliser les parenthèses pour les priorités de calcul
