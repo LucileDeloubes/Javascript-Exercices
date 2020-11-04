51a073175772785ed31783e12a605df99e5c8cea
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-04 12:00:23 +0100

    correction JS partie 2 exo 1

--- Partie 2 - exo 1/script.js
+++ Partie 2 - exo 1/script.js
@@ -1,7 +1,10 @@
-function setImg1() {
-    document.getElementById('booGhost').src="IMG/Boo_Hide.png";
+var image = document.getElementById('booGhost');
+image.onmouseover = function changeImage() {
+    image.src="IMG/Boo_Hide.png";
 }
-function setImg2() {
-    document.getElementById('booGhost').src="IMG/Boo.png";
+image.onmouseout = function mainImage() {
+    image.src="IMG/Boo.png";
 }
 // On appelle les fonctions setIlmg et on leur demande de changer l'image
+
+// Si le fichier JS est dans un autre dossier, partir du principe que lorsqu'on modifie une image dans l'html, le chemin de l'image part de la même racine que l'index.
